6c4b65e39dab0228f6419e40ddb4877b77e27046
Lucas Souza <lucasfs7@gmail.com>
2014-02-27 23:22:02 -0300

    remove coffee compile task and add js app concat task

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,26 @@ var gulp = require('gulp');
 var util = require('gulp-util');
 var jade = require('gulp-jade');
 var stylus = require('gulp-stylus');
-var coffee = require('gulp-coffee');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
+var path = {
+  jade: './src/**/*.jade',
+  stylus: './src/stylus/**/*.styl',
+  scripts: {
+    app: './src/scripts/app/**/*.js',
+    vendor: './src/scripts/vendor/**/*.js'
+  }
+};
+
 gulp.task('compile:html', function() {
-  gulp.src('./src/**/*.jade')
+  gulp.src(path.jade)
     .pipe(jade())
     .pipe(gulp.dest('./build/'))
 });
 
 gulp.task('compile:css', function () {
-  gulp.src('./src/stylus/**/*.styl')
+  gulp.src(path.stylus)
     .pipe(stylus({
       set:['compress'],
       use: ['nib']
@@ -21,29 +29,25 @@ gulp.task('compile:css', function () {
     .pipe(gulp.dest('./build/stylesheets'));
 });
 
-gulp.task('compile:javascript', function() {
-  gulp.src('./src/coffee/**/*.coffee')
-    .pipe(coffee({
-      bare: true,
-      join: true
-    })
-    .on('error', util.log))
+gulp.task('concat:scripts:app', function() {
+  gulp.src(path.scripts.app)
+    .pipe(concat("app.js"))
     .pipe(uglify({outSourceMap: true}))
-    .pipe(gulp.dest('./build/scripts'))
+    .pipe(gulp.dest('./build/scripts/'))
 });
 
-gulp.task('concat:vendor', function() {
-  gulp.src('./src/vendor/scripts/**/*.js')
+gulp.task('concat:scripts:vendor', function() {
+  gulp.src(path.scripts.vendor)
     .pipe(concat("vendor.js"))
     .pipe(uglify({outSourceMap: true}))
     .pipe(gulp.dest('./build/scripts/'))
 });
 
-gulp.task('build', ['compile:html', 'compile:css', 'compile:javascript', 'concat:vendor']);
+gulp.task('build', ['compile:html', 'compile:css', 'concat:scripts:app', 'concat:scripts:vendor']);
 
 gulp.task('default', ['build'], function(){
-  gulp.watch('./src/**/*.jade', ['compile:html']);
-  gulp.watch('./src/stylus/**/*.styl', ['compile:css']);
-  gulp.watch('./src/coffee/**/*.coffee', ['compile:javascript']);
-  gulp.watch('./src/vendor/scripts/**/*.js', ['concat:vendor']);
+  gulp.watch(path.jade, ['compile:html']);
+  gulp.watch(path.stylus, ['compile:css']);
+  gulp.watch(path.scripts.app, ['concat:scripts:app']);
+  gulp.watch(path.scripts.vendor, ['concat:scripts:vendor']);
 });
