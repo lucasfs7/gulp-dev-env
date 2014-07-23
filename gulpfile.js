1b8b1dda66c6f3650c2a1017293c372edd6bb03c
Lucas F. Souza <lucasfs7@gmail.com>
2014-07-23 13:13:08 -0300

    - improve gulp tasks - improve npm scripts - allow compile for production with min and uglify - allow watch files without compression

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,9 @@ var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
 var imagemin = require('gulp-imagemin');
 var rename = require('gulp-rename');
+var argv = require('minimist')(process.argv.slice(2));
+var minifyCSS = require('gulp-minify-css');
+var defineModule = require('gulp-define-module');
 
 var path = {
   layouts: './src/layouts/**/*.jade',
@@ -15,10 +18,11 @@ var path = {
   stylus: './src/stylus/*.styl',
   stylusAll: './src/stylus/**/*.styl',
   scripts: {
-    app: './src/scripts/app/**/*.js',
+    app: './src/scripts/app/**/*.ls',
     vendor: './src/scripts/vendor/**/*.js',
     modules: {
-      main: './src/scripts/app/main.ls'
+      main: './src/scripts/app/main.ls',
+      templates: './src/scripts/app/templates/**/*.jade'
     }
   },
   images: [
@@ -29,36 +33,54 @@ var path = {
 };
 
 gulp.task('compile:html', function() {
-  gulp.src([path.jade, '!'+path.layouts])
+  gulp.src([path.jade, '!'+path.layouts, '!'+path.scripts.modules.templates])
     .pipe(jade())
     .pipe(gulp.dest('./build/'))
 });
 
 gulp.task('compile:css', function () {
-  gulp.src(path.stylus)
-    .pipe(stylus({
-      set:['compress'],
-      use: [nib()]
-    }))
-    .pipe(gulp.dest('./build/stylesheets'));
+  var t = gulp.src(path.stylus);
+  t = t.pipe(stylus({use: [nib()]}));
+
+  if (argv.c) {
+    t = t.pipe(minifyCSS());
+  }
+
+  t = t.pipe(gulp.dest('./build/stylesheets'));
 });
 
-gulp.task('browserify:app', function() {
-  gulp.src(path.scripts.modules.main, { read: false })
-    .pipe(browserify({
-      transform: ['liveify'],
-      extensions: ['.ls']
-    }))
-    .pipe(uglify())
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest('./build/scripts'))
+gulp.task('browserify:app', ['compile:templates'], function() {
+  var t = gulp.src(path.scripts.modules.main, { read: false });
+
+  t = t.pipe(browserify({
+    transform: ['liveify'],
+    extensions: ['.ls']
+  }));
+
+  if (argv.u) {
+    t = t.pipe(uglify());
+  }
+
+  t = t.pipe(rename('app.js'));
+  t = t.pipe(gulp.dest('./build/scripts'));
 });
 
 gulp.task('concat:scripts:vendor', function() {
-  gulp.src(path.scripts.vendor)
-    .pipe(concat("vendor.js"))
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/scripts/'))
+  var t = gulp.src(path.scripts.vendor);
+  t = t.pipe(concat("vendor.js"));
+
+  if (argv.u) {
+    t = t.pipe(uglify());
+  }
+
+  t = t.pipe(gulp.dest('./build/scripts/'));
+});
+
+gulp.task('compile:templates', function() {
+  var t = gulp.src(path.scripts.modules.templates);
+  t = t.pipe(jade({client: true}));
+  t = t.pipe(defineModule('node'));
+  t = t.pipe(gulp.dest('./src/scripts/app/templates'));
 });
 
 gulp.task('compress:images', function() {
@@ -71,8 +93,9 @@ gulp.task('build', ['compile:html', 'compile:css', 'browserify:app', 'concat:scr
 
 gulp.task('watch', function() {
   gulp.watch(path.jade, ['compile:html']);
+  gulp.watch(path.scripts.modules.templates, ['compile:templates', 'browserify:app']);
   gulp.watch(path.stylusAll, ['compile:css']);
-  gulp.watch(path.scripts.app, ['concat:scripts:app']);
+  gulp.watch(path.scripts.app, ['browserify:app']);
   gulp.watch(path.scripts.vendor, ['concat:scripts:vendor']);
   gulp.watch(path.images, ['compress:images']);
 });
